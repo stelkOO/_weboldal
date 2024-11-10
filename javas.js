8100bec8ca54632d7da96b6e1eee947767eb4bb1
stelkOO <156014089+stelkOO@users.noreply.github.com>
2024-11-10 14:01:50 +0100

    Add files via upload

---
+++ javas.js
@@ -0,0 +1,4 @@
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+});
